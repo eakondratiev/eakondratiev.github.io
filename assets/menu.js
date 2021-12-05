3dfd2a7366cff4ea605c8bf9b44eaf096bcb3549
E. Kondratiev <ekondr@hotmail.com>
2021-12-05 18:17:49 +0300

    Browser compatibility check, menu.js minified.

--- assets/menu.js
+++ assets/menu.js
@@ -2,12 +2,26 @@
  * The site menu functions.
  * 
  * 2020-12-26
+ * 2021-12-05 browser compatibility tests added.
  */
-
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// @output_file_name menu.min.js
+// ==/ClosureCompiler==
 (function () {
 
   // initialize
   window.onload = function (event) {
+
+    // check for compatibility
+    if (document.getElementsByClassName === undefined ||
+        window.addEventListener === undefined ||
+        window.getComputedStyle === undefined) {
+
+      document.getElementById('incompatible-browser').style.display = 'block';
+      return;
+
+    }
 
     var menuBtn = document.getElementsByClassName('site-menu-btn')[0],
       menu = document.getElementsByClassName('site-nav')[0];
