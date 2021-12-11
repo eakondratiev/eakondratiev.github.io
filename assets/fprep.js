d383cce8bf42b3e5e4f3026712138e570b2369bd
E. Kondratiev <ekondr@hotmail.com>
2021-12-11 21:45:36 +0300

    The fetch() detection added; processing 64-bit double;

--- assets/fprep.js
+++ assets/fprep.js
@@ -9,6 +9,7 @@
  * 2021-12-06
  * 2021-12-08 use of T.getNumber().
  * 2021-12-09 special case of zero.
+ * 2021-12-11 code corrections, 64-bit (double) added.
  */
 // ==ClosureCompiler==
 // @compilation_level SIMPLE_OPTIMIZATIONS
@@ -19,13 +20,36 @@
  */
 function processFloatingPointValue(inputElement, resultElement) {
 
+  const BITS = 32; // 32 or 64
+
+  // TEST
+  // 0,1
+  // 32-bit
+  // 001111011 1001100 11001100 11001100 (DONE)
+  // 64-bit
+  // 00111111 10111001 10011001 10011001 10011001 10011001 10011001 10011001
+
   var inputText = inputElement.value;
   var number = T.getNumber(inputText);
+  var exponentBits;
+  var exponentBias;
+
+  if (BITS === 64) {
+    exponentBits = 11;
+    exponentBias = 1023;
+  }
+  else {
+    exponentBits = 8;
+    exponentBias = 127;
+  }
 
   if (isNaN(number) || !isFinite(number)) {
     resultElement.innerHTML = 'n/a';
     return;
   }
+
+  // the title
+  resultElement.innerHTML = '<h2>' + BITS + '-bit representation</h2>';
 
   if (number === 0) {
     // special case
@@ -33,11 +57,11 @@ function processFloatingPointValue(inputElement, resultElement) {
     var zeroes = [];
     var i;
 
-    for (i = 0; i < 32; i++) zeroes[i] = 0;
+    for (i = 0; i < BITS; i++) zeroes[i] = 0;
 
-    resultElement.innerHTML = '<h2>32-bit representation</h2>' +
+    resultElement.innerHTML +=
       '<p>Zero is a <b>special case</b>. It represented by special bit pattern of all zeroes.</p>' +
-      '<p>' + formatBits (zeroes) + '</p>';
+      '<p>' + formatBits (zeroes, exponentBits) + '</p>';
 
     return;
   }
@@ -49,18 +73,21 @@ function processFloatingPointValue(inputElement, resultElement) {
     .then(bytes => WebAssembly.instantiate(bytes))
     .then(results => {
 
-      const BITS = 32;
       var instance = results.instance;
       var i;
       var floatValue;
 
-      const { memory, getFloatBits } = instance.exports;
+      const { memory, getFloatBits, getDoubleBits } = instance.exports;
       const arr = new Int32Array(memory.buffer, 0, BITS);
 
-      getFloatBits(arr.byteOffser, number);
+      if (BITS === 64) {
+        getDoubleBits(arr.byteOffser, number);
+      }
+      else {
+        getFloatBits(arr.byteOffser, number);
+      }
 
-      resultElement.innerHTML = '<h2>32-bit representation</h2>' +
-        '<div class="bits">' + formatBits (arr) + '</div>';
+      resultElement.innerHTML += '<div class="bits">' + formatBits (arr, exponentBits) + '</div>';
 
       // bits description
       // the sign bit
@@ -86,7 +113,7 @@ function processFloatingPointValue(inputElement, resultElement) {
 
       description = 'The exponent stored as <span class="fp-bit fp-exp">';
 
-      for (i = 1; i <= 8; i++) {
+      for (i = 1; i <= exponentBits; i++) {
         expValue += arr[i];
       }
 
@@ -94,8 +121,8 @@ function processFloatingPointValue(inputElement, resultElement) {
 
       expValue = parseInt(expValue, 2); // convert to decimal
       description += '<b>' + expValue + '</b> and the real value is ' +
-        expValue + ' - 127 = <b>' + (expValue - 127) + '</b>. ' +
-        'So the exponent is <b>2<sup>' + (expValue - 127) + '</sup></b>.';
+        expValue + ' - ' + exponentBias + ' = <b>' + (expValue - exponentBias) + '</b>. ' +
+        'So the exponent is <b>2<sup>' + (expValue - exponentBias) + '</sup></b>.';
 
       resultElement.innerHTML += '<p>' + description + '</p>';
 
@@ -105,7 +132,7 @@ function processFloatingPointValue(inputElement, resultElement) {
 
       description = 'The fraction stored as <span class="fp-bit fp-fraction">';
 
-      for (i = 9; i < BITS; i++) {
+      for (i = exponentBits + 1; i < BITS; i++) {
         fraction += arr[i];
         fValue += arr[i] * Math.pow(2, 8 - i);
       }
@@ -117,13 +144,13 @@ function processFloatingPointValue(inputElement, resultElement) {
       resultElement.innerHTML += '<p>' + description + '</p>';
 
       // So
-      floatValue *= (1 + fValue) * Math.pow(2, (expValue - 127));
+      floatValue *= (1 + fValue) * Math.pow(2, (expValue - exponentBias));
 
       resultElement.innerHTML += '<p>The number <b>' + number +
         '</b> is represented as <b>' + fpSign + (1 + fValue) + '&times;2<sup>' +
-        (expValue - 127) + '</sup> = <b>' + floatValue + '</b></p > ';
+        (expValue - exponentBias) + '</sup> = <b>' + floatValue + '</b></p > ';
 
-      resultElement.innerHTML += '<p>The javascript representation is <b>' +
+      resultElement.innerHTML += '<p>The javascript representation is 64-bits <b>' +
         number.toFixed(BITS) + '</b>.</p>';
 
     })
@@ -132,20 +159,24 @@ function processFloatingPointValue(inputElement, resultElement) {
   /**
    * Returns html code for the bits.
    * @param {[number]} bits the array of bits.
+   * @param {number} exponentBits the number of bits for exponent, deafult is 8.
    * @returns {string}
    */
-  function formatBits(bits) {
+  function formatBits(bits, exponentBits) {
 
     var i,
         text = '';
+    
+    exponentBits = exponentBits || 8;
 
     // display bits
     for (i = 0; i < bits.length; i++) {
 
-      text += formatBit(bits[i], i);
+      text += formatBit(bits[i], i, exponentBits);
 
       // separate octets with a space, so the line could be break down.
-      if (i === 7 || i === 15 || i === 23 || i === 31) {
+      if (i === 7 || i === 15 || i === 23 || i === 31 ||
+          i === 39 || i === 47 || i === 55 || i === 63) {
         text += ' ';
       }
 
@@ -159,15 +190,18 @@ function processFloatingPointValue(inputElement, resultElement) {
    * Returns formatted bit.
    * @param {number} value the bit value.
    * @param {number} index the bit index in array.
+   * @param {number} exponentBits the number of bits for exponent, deafult is 8.
    */
-  function formatBit(value, index) {
+  function formatBit(value, index, exponentBits) {
 
     var css;
+
+    exponentBits = exponentBits || 8;
 
     if (index === 0) {
       css = 'fp-sign';
     }
-    else if (index <= 8) {
+    else if (index <= exponentBits) {
       css = 'fp-exp';
     }
     else {
