e51b4a7de82cd23ee4fea419dd7d445b5aaef25b
E. Kondratiev <ekondr@hotmail.com>
2022-05-16 20:41:17 +0300

    numbers to Roman numerals html and js

--- assets/roman-numerals.js
+++ assets/roman-numerals.js
@@ -4,6 +4,7 @@
  * 2022-05-10 errors handling added.
  * 2022-05-11 keyboard handler and other corrections.
  * 2022-05-15 romanToInt - validation code moved.
+ * 2022-05-16 number to Roman numeral conversion added.
  */
 // ==ClosureCompiler==
 // @compilation_level SIMPLE_OPTIMIZATIONS
@@ -19,11 +20,19 @@ function RomanNumerals() {
   var MAX_INPUT_LENGTH = 15, // characters
     reValid = /^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/i, // thanks to https://stackoverflow.com/a/267405
     inputRomanNumber = document.getElementById('roman-number'),
-    resultElement = document.getElementById('result'),
     resultBlock = document.getElementById('result-block'),
+    resultElement = document.getElementById('result'),
     messageBlock = document.getElementById('message-block'),
     btnRomanToNumber = document.getElementById('button-to-number'),
     message = new T.Message ({element: document.getElementById('message-block')}),
+
+    inputIntegerNumber = document.getElementById ('integer-number'),
+    resultBlockN2r = document.getElementById ('result-block-n2r'),
+    resultElementN2r = document.getElementById ('result-n2r'),
+    messageBlogN2r = document.getElementById ('message-block-n2r'),
+    btnNumberToRoman = document.getElementById ('button-to-roman'),
+    messageN2r = new T.Message ({element: document.getElementById('message-block-n2r')}),
+
     urlParams = T.getUrlParameters();
 
   //test ();
@@ -42,14 +51,25 @@ function RomanNumerals() {
 
   }
 
+  if (urlParams.number !== undefined) {
+
+    inputIntegerNumber.value = decodeURIComponent (urlParams.number);
+    convertNumberToRoman ();
+
+  }
+
   // Event handlers
   inputRomanNumber.addEventListener('keyup', convertRomanToNumber);
   btnRomanToNumber.addEventListener('click', convertRomanToNumber);
 
+  inputIntegerNumber.addEventListener('keyup', convertNumberToRoman);
+  btnNumberToRoman.addEventListener('click', convertNumberToRoman);
+
   /**
-    * Clears the result text and styles, hide messages.
+    * Clears the result text and styles and hide messages
+    * in the Roman to number block.
     */
-  function clearResults() {
+  function clearResultsR2N() {
 
     resultBlock.style.display = 'block';
     resultElement.innerHTML = '';
@@ -58,7 +78,19 @@ function RomanNumerals() {
   }
 
   /**
-    * Converts a roman number to integer number.
+    * Clears the result text and styles and hide messages
+    * in the Number to Roman block.
+    */
+  function clearResultsN2R() {
+
+    resultBlockN2r.style.display = 'block';
+    resultElementN2r.innerHTML = '';
+    messageN2r.hide ();
+
+  }
+
+  /**
+    * Converts a roman numeral to an integer number.
     * @param (*} event the DOM event, optional.
     */
   function convertRomanToNumber(event) {
@@ -83,10 +115,10 @@ function RomanNumerals() {
 
     }
 
-    clearResults ();
+    clearResultsR2N ();
 
     // convert
-    romanNumber = inputRomanNumber.value.replace (/^\s+|\s+$/gm, ''); // get text adt trim spaces
+    romanNumber = inputRomanNumber.value.replace (/^\s+|\s+$/gm, ''); // get text and trim spaces
     result = romanToInt (romanNumber, MAX_INPUT_LENGTH);
 
     // output
@@ -129,6 +161,75 @@ function RomanNumerals() {
     inputRomanNumber.style.border = inputBorderStyle;
     resultElement.innerHTML = html;
 
+  }
+
+  /**
+    * Converts a number to Roman numeral.
+    * @param (*} event the DOM event, optional.
+    */
+  function convertNumberToRoman(event) {
+
+    var integerNumber,
+      inputText,
+      result,
+      html = '',
+      inputBorderStyle = '',
+      key;
+
+    if (event !== undefined) {
+
+      key = event.keyCode;
+
+      if (key !== undefined && key !== 13) {
+        // A key was pressed by not Enter
+        return;
+      }
+
+      // Cancel the default action, if needed
+      event.preventDefault();
+
+    }
+
+    clearResultsN2R ();
+
+    // convert
+    inputText = inputIntegerNumber.value
+        .replace (/^\s+|\s+$/gm, '') // get text and trim spaces
+        .replace (/[,]/g, '.');      // replace the decimal delimiter to a dot
+
+    integerNumber = parseFloat (inputText); // convert to float, the function will test is in integer or float
+    result = intToRoman (integerNumber);
+
+    // output
+    switch (result.error) {
+      case 0:
+        html = '<b>' + integerNumber + '</b> = <b>' + result.roman + '</b>';
+        break;
+
+      case 1:
+        messageN2r.show (
+          'The text "<span style="color:#c00;">' + inputText +'</span>" is not a number.',
+          T.MessageLevel.WARNING);
+        break;
+
+      case 2:
+        messageN2r.show ('Only integer numbers from 1 to 3999 are supported.', T.MessageLevel.WARNING);
+        break;
+
+      case 3:
+        messageN2r.show (
+          'The text "<span style="color:#c00;">' + inputText +'</span>" is not recognized as an integer number.',
+          T.MessageLevel.WARNING);
+        break;
+
+      default:
+        inputBorderStyle = '1px solid #e00';
+        messageN2r.show ('Unexpected value.', T.MessageLevel.ERROR);
+        break;
+    }
+
+    inputIntegerNumber.style.border = inputBorderStyle;
+    resultElementN2r.innerHTML = html;
   }
 
   /**
@@ -233,6 +334,79 @@ function RomanNumerals() {
     }
         
     return {n:number, error: 0};
+  }
+
+  /**
+    * Returns the structure with the Roman numeral and an error code.
+    * Error codes: 0 - ok, 1 - not a number, 2 - Not in range 1...3999,
+    *   3 - Not integer
+    * @param {number} n the input value, a number.
+    * @return { {roman:string, error:number} }
+    */
+  function intToRoman (n) {
+
+    var symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M'],
+      index = 0,
+      chars,
+      i,
+      digit,
+      result = '';
+
+    // validation
+    if (typeof (n) !== 'number' || isNaN (n)) {
+      return { roman:'', error: 1 }; // Not a number
+    }
+
+    if (n < 1 || n > 3999) {
+      return { roman:'', error: 2 }; // Not in range 1...3999
+    }
+
+    if (n !== Math.floor (n)) {
+      return { roman:'', error: 3 }; // Not integer
+    }
+
+    chars = n.toString().split('');
+
+    for (i = chars.length - 1; i >= 0; i--) {
+
+      digit = parseInt (chars[i], 10);
+
+      switch (digit) {
+      case 1:
+        result = symbols[index] + result;
+        break;
+      case 2:
+        result = symbols[index] + symbols[index] + result;
+        break;
+      case 3:
+        result = symbols[index] + symbols[index] + symbols[index] + result;
+        break;
+      case 4:
+        result = symbols[index] + symbols[index + 1] + result;
+        break;
+      case 5:
+        result = symbols[index + 1] + result;
+        break;
+      case 6:
+        result = symbols[index + 1] + symbols[index] + result;
+        break;
+      case 7:
+        result = symbols[index + 1] + symbols[index] + symbols[index] + result;
+        break;
+      case 8:
+        result = symbols[index + 1] + symbols[index] + symbols[index] + symbols[index] + result;
+        break;
+      case 9:
+        result = symbols[index] + symbols[index + 2] + result;
+        break;
+
+      }
+
+      index += 2;
+
+    }
+
+    return { roman: result, error: 0 };
   }
 
   /**
